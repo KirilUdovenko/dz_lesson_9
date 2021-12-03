2d272fb818b3f8e84baf779a01fd2eb6a6f10357
KirilUdovenko <kiril.stuff1@gmail.com>
2021-12-04 01:14:40 +0200

    neponyatno

--- dz_lesson_9.py
+++ dz_lesson_9.py
@@ -1,54 +1,86 @@
-def read_file(filename):
-    with open(filename, 'r') as file:
-        return file.read().split('\n')
+class FileWorkerReplace:
+    def __init__(self, filename_domains):
+        self.filename_domains = filename_domains
+        self.read_file()
+        self.get_domains()
 
+    def read_file(self):
+        with open(self.filename_domains, 'r') as file:
+            return file.read().split('n')
 
-def get_domains(filename):
-    lines = read_file(filename)
-    lines = [line.replace(".", "") for line in lines]
-    return lines
-
-
-def get_names(filename):
-    lines = read_file(filename)
-    lines = [line.split("\t")[1] for line in lines]
-    return lines
-
-
-def str_to_date(some_str):
-    months_dict = {'January': '01',
-                   'February': '02',
-                   'March': '03',
-                   'April': '04',
-                   'May': '05',
-                   'June': '06',
-                   'July': '07',
-                   'August': '08',
-                   'September': '09',
-                   'October': '10',
-                   'November': '11',
-                   'December': '12'}
-    parts = some_str.split()
-    day, month, year = parts
-    date = f"{day[:-2].zfill(2)}/{months_dict[month]}/{year}"
-    return date
-
-
-def get_dates(filename):
-    lines = read_file(filename)
-    lines = [line.split(" - ")[0] for line in lines if " - " in line]
-    lines = [str_to_date(line) for line in lines]
-    return lines
+    def get_domains(self):
+        lines = self.read_file()
+        lines = [line.replace(".", "") for line in lines]
+        return lines
 
 
 filename_domains = "domains.txt"
-domains = get_domains(filename_domains)
-print(domains)
+file_worker_replace = FileWorkerReplace(filename_domains)
+file_worker_replace.read_file()
+file_worker_replace.get_domains()
+
+
+class FileWorkerSort:
+    def __init__(self, filename_names):
+        self.filename_names = filename_names
+        self.read_file()
+        self.get_names()
+
+    def read_file(self):
+        with open(self.filename_names, 'r') as file:
+            return file.read().split('n')
+
+    def get_names(self):
+        lines = self.read_file()
+        lines = (line.split("\t")[1] for line in lines)
+        return lines
+
 
 filename_names = "names.txt"
-names = get_names(filename_names)
-print(names)
+file_worker_sort = FileWorkerSort(filename_names)
+file_worker_sort.read_file()
+file_worker_sort.get_names()
+
+
+class DateRemake:
+    def __init__(self, filename_dates):
+        self.filename_dates = filename_dates
+        self.read_file()
+        self.get_dates()
+
+    def read_file(self):
+        with open(self.filename_dates, 'r') as file:
+            return file.read().split('n')
+
+    def str_to_date(some_str):
+        months_dict = {'January': '01',
+                       'February': '02',
+                       'March': '03',
+                       'April': '04',
+                       'May': '05',
+                       'June': '06',
+                       'July': '07',
+                       'August': '08',
+                       'September': '09',
+                       'October': '10',
+                       'November': '11',
+                       'December': '12'}
+        parts = some_str.split()
+        day, month, year = parts
+        date = f"{day[:-2].zfill(2)}/{months_dict[month]}/{year}"
+        return date
+
+    def get_dates(self):
+        lines = self.read_file()
+        lines = [line.split(" - ")[0] for line in lines if " - " in line]
+        lines = [str_to_date(line) for line in lines]
+        return lines
+
 
 filename_dates = "author.txt"
-dates = get_dates(filename_dates)
-print(dates)
+date_remake = DateRemake(filename_dates)
+date_remake.read_file()
+date_remake.get_dates()
+
+# dates = get_dates(filename_dates)
+# print(dates)
